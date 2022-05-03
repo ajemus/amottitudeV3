e40e4878af4f6369f540c79d23e41f90bd565742
Larcherbc <jules.rbc@gmail.com>
2022-05-03 20:55:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/logo_eravys.png" />
+	<title>Amottitude V3</title>
+</head>
+<body>
+	<header>
+		<h1 id="title"><a href="index.php">a<em class="orangeSlash">(</em>mo<em class="orangeSlash">)</em>tittude</a></h1>
+		<nav>
+			<a href="reference.php" class="link">références</a>
+			<a href="index.php#contact" class="link">contact</a>
+		</nav>
+	</header>
+
+	<section id="presentationSection">
+		<article class="boxPresentation">
+			<p class="texte">A(mo)ttitude est une société d'Assistance à Maitrise d'Ouvrage qui a la bonne attitude : écoute analytique, observations précises et synthétiques, recherche exhaustive des contraintes environnementales, sociales, urbaine, réglementaires, économiques, fonctionnelles, spatiales et opérationnelles</p>
+
+			<div class="boxSavoirFaire">
+				<p class="savoirFaire" id="savoirFaire1">Assistance à maîtrise d'ouvrage <em class="orangeSlash">/</em> Programmation architecturale et technique <em class="orangeSlash">/</em></p>
+			</div>
+			<div class="boxSavoirFaire">
+				<p class="savoirFaire" id="savoirFaire2">Qualité d'usage <em class="orangeSlash">/</em> Accessibilité <em class="orangeSlash">/</em> Concertation des utilisateurs et usagers</p>
+			</div>
+
+			<p class="texte">Nous permettons à nos clients de cadrer leur projet, de faire en sorte que leur bâtiment projeté corresponde aussi bien aux attentes des décideurs que des usagers et utilisateurs de l'équipement, tout en maîtrisant les délais, les coûts et les risques</p>
+			<div class="boxBorder"></div>
+			<div class="boxBorder"></div>
+			<div class="boxBorder"></div>
+		</article>
+		<article class="boxPresentation">
+
+		</article>
+	</section>
+	<section id="referencesSection">
+
+	</section>
+	<section id="contactSection">
+
+	</section>
+	<footer>
+
+	</footer>
+	<!-- <script src="assets/*.js"></script> -->
+
+</body>
+</html>
