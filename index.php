c31f2a3e553f743434febd5586fb5bec524c205f
Larcherbc <jules.rbc@gmail.com>
2022-05-04 08:32:28 +0200

    mise en forme partie présentation

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 	<section id="presentationSection">
 		<article class="boxPresentation">
-			<p class="texte">A(mo)ttitude est une société d'Assistance à Maitrise d'Ouvrage qui a la bonne attitude : écoute analytique, observations précises et synthétiques, recherche exhaustive des contraintes environnementales, sociales, urbaine, réglementaires, économiques, fonctionnelles, spatiales et opérationnelles</p>
+			<p class="texte" id="texte1">A(mo)ttitude est une société d'Assistance à Maitrise d'Ouvrage qui a la bonne attitude : écoute analytique, observations précises et synthétiques, recherche exhaustive des contraintes environnementales, sociales, urbaine, réglementaires, économiques, fonctionnelles, spatiales et opérationnelles.</p>
 
 			<div class="boxSavoirFaire">
 				<p class="savoirFaire" id="savoirFaire1">Assistance à maîtrise d'ouvrage <em class="orangeSlash">/</em> Programmation architecturale et technique <em class="orangeSlash">/</em></p>
@@ -27,12 +27,26 @@
 				<p class="savoirFaire" id="savoirFaire2">Qualité d'usage <em class="orangeSlash">/</em> Accessibilité <em class="orangeSlash">/</em> Concertation des utilisateurs et usagers</p>
 			</div>
 
-			<p class="texte">Nous permettons à nos clients de cadrer leur projet, de faire en sorte que leur bâtiment projeté corresponde aussi bien aux attentes des décideurs que des usagers et utilisateurs de l'équipement, tout en maîtrisant les délais, les coûts et les risques</p>
-			<div class="boxBorder"></div>
-			<div class="boxBorder"></div>
-			<div class="boxBorder"></div>
+			<p class="texte" id="texte2">Nous permettons à nos clients de cadrer leur projet, de faire en sorte que leur bâtiment projeté corresponde aussi bien aux attentes des décideurs que des usagers et utilisateurs de l'équipement, tout en maîtrisant les délais, les coûts et les risques.</p>
+			<div class="boxBorder" id="boxBorder1"></div>
+			<div class="boxBorder" id="boxBorder2"></div>
+			<div class="boxBorder" id="boxBorder3"></div>
 		</article>
 		<article class="boxPresentation">
+			<div class="boxBorder" id="boxBorder4"></div>
+			<div class="boxBorder" id="boxBorder5"></div>
+			<div class="boxBorder" id="boxBorder6"></div>
+			<div  id="whiteBg"></div>
+			<p id="customSlash" class="orange">//</p>
+			<p class="texte" id="texte3">Outre la clarification des besoins, des enjeux et des exigences de l'opération, la consolidation des orientations et faisabilité, l'ajustement des contraintes, la préprogrammation et la programmation fonctionnelle et technique proprement dite, nous pouvons accompagner nos clients dans les phases de choix du concepteur d'études de conception et de chantier.</p>
+			<p class="texte" id="texte4">Sa gérante, Dorothée-Lân RABEC,
+			architecte de formation et programmiste généraliste, a plus de 20 ans d'expérience et de nombreuse références en programmation architecturale dans des domaines très divers (équipement universitaires et scolaires, équipements petite enfance, logements, bâtiments médico-sociaux et de recherche, équipements culturels, équipements sportifs, bâtiments tertiaires et industriels,...). Elle a également à son actif de nombreuses réhabilitations de bâtiments, des schémas directeurs, des études d'opportunités, des diagnostics, accessibilités, ...</p>
+			<div id="titre">
+				<p class="texte orange" id="texte5">Dorothée-Lân RABEC</p>
+				<p class="texte" id="texte6">Dirigente-Fondatrice</p>
+			</div>
+			<img src="assets/images/photo_portrait.jpg" id="photoPortrait">
+			
 
 		</article>
 	</section>
